f0c4e30dd1fe70cbf2d973ff3cf26f2b452d6de5
Claire Lee <jclairelee@gmail.com>
2023-02-01 09:37:16 -0800

    Change size in CSS

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ const cursor = document.querySelector(" .hammer");
 const holes = [...document.querySelectorAll(" .hole")];
 const scoreEl = document.querySelector(" .score span");
 let score = 0;
+let timer = null;
 const hittingSound = new Audio("./sound/hit.mp3");
 
 function run() {
   const randomNum = Math.floor(Math.random() * holes.length);
   const hole = holes[randomNum];
-  let timer = null;
   const img = document.createElement("img");
+
   img.classList.add("mole");
   img.src = "./image/mole.png";
 
-  img.addEventListener("click", () => {
-    score += 10;
+  img.addEventListener("click", (e) => {
     hittingSound.play();
+    score += 5;
     scoreEl.textContent = score;
     img.src = "./image/whack-a-mole.png";
     clearTimeout(timer);
@@ -24,20 +25,24 @@ function run() {
       run();
     }, 500);
   });
+
   hole.appendChild(img);
   timer = setTimeout(() => {
     hole.removeChild(img);
     run();
-  }, 1300);
+  }, 1500);
 }
 run();
+
 window.addEventListener("mouseover", (e) => {
   cursor.style.top = e.pageY + "px";
   cursor.style.left = e.pageX + "px";
 });
+
 window.addEventListener("mousedown", () => {
   cursor.classList.add("active");
 });
+
 window.addEventListener("mouseup", () => {
   cursor.classList.remove("active");
 });
