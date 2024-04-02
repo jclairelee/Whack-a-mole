af4e7dafe32fd91cb8636975c6bede9c0271727f
Claire Lee <jclairelee@gmail.com>
2024-04-01 23:02:33 -0700

    Add timer and btn

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
+const startBtn = document.getElementById("startButton");
+const content = document.getElementById("game-content");
+const descEl = document.querySelector(" .desc");
+const timerEl = document.querySelector(" .timer span");
+let timer = 180;
 const cursor = document.querySelector(" .hammer");
 //total nine holes (3X3)
 const holes = [...document.querySelectorAll(" .hole")];
 const scoreEl = document.querySelector(" .score span");
 let score = 0;
-let timer = null;
 const hittingSound = new Audio("./sound/hit.mp3");
+
+startBtn.addEventListener("click", function () {
+  descEl.style.display = "none";
+  startBtn.style.display = "none";
+
+  content.style.display = "block";
+  console.log(timerEl.innerHTML);
+
+  const countdown = setInterval(function () {
+    let secondsLeft = parseInt(timerEl.textContent) - 1;
+    timerEl.textContent = secondsLeft;
+
+    if (secondsLeft <= 0) {
+      clearInterval(timer);
+
+      alert("Time's up!");
+    }
+  }, 1000);
+});
 
 function run() {
   const randomNum = Math.floor(Math.random() * holes.length);
